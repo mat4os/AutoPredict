9840bfdc9fd8f952882c9ab34773fd2b94723b63
mat4os <mat4os@gmx.es>
2016-06-19 13:32:36 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='autopredict',
+      version='0.1',
+      description='Programa para score de crédito',
+      url='http://github.com/mat4os/autopredict',
+      author='Cerdan&Garrido&Nuño',
+      author_email='mat4os@gmx.es',
+      license='MIT',
+      packages=['autopredict'],
+      zip_safe=False)
